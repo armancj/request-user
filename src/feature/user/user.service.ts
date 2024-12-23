c646f754f9fe821d5a1f1aa4667b8b7bccccdbae
mandi <armancanjer@gmail.com>
2024-12-23 13:50:57 -0500

    feat: add concole log to main server

--- src/feature/user/user.service.ts
+++ src/feature/user/user.service.ts
@@ -1,15 +1,16 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { User } from './entity/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
-import * as console from 'console';
-
 
 @Injectable()
 export class UserService {
-  private users: User[] = [{
-    id: 1, edad: 60, name: 'mandi'
-  }];
-
+  private users: User[] = [
+    {
+      id: 1,
+      edad: 60,
+      name: 'mandi',
+    },
+  ];
 
   findAllUser(): User[] {
     return this.users;
@@ -18,27 +19,31 @@ export class UserService {
   create(createUserDto: CreateUserDto) {
     const user = {
       ...createUserDto,
-      id: this.users.length + 1
-    }
+      id: this.users.length + 1,
+    };
     this.users.push(user);
-    return createUserDto;
+    return user;
   }
 
-  findOne(id: number) {
-     const user = this.users[id-1]
+  findOne(id: number): User {
+    const user = this.users.find((user) => user.id === id);
 
-    if (!user) throw new NotFoundException('user not found');
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
 
     return user;
   }
 
   delete(id: number) {
-    const index = this.users.findIndex(user => user.id === id);
+    const index = this.users.findIndex((user) => user.id === id);
 
-    if (index === -1) { throw new NotFoundException(`User with ID ${id} not found`); }
+    if (index === -1) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
 
     this.users.splice(index, 1);
 
-    return { mensage: 'delete successfully' };
+    return { message: 'Deleted successfully' };
   }
 }
