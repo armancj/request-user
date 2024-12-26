e1acd7b5c6aef8df3d76178d4e5e3503b0c5ec21
Hector <hectikorf@gmail.com>
2024-12-25 22:44:01 -0500

    Books Module add to app.module without error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { BooksModule } from './feature/books/books.module';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './feature/user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule],
+  imports: [ConfigModule.forRoot(), UserModule,BooksModule],
   controllers: [AppController],
   providers: [],
 })
