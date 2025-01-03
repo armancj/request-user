c5395d16c10b86417e0eef41878db402ba8f86a4
jennidalila <jrodriguezaloma@gmail.com>
2025-01-03 18:03:12 -0500

    Implementación de CRUD para Library

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { BooksModule } from './feature/books/books.module';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './feature/user/user.module';
+import { LibraryModule } from './feature/library/library.module';
+
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule,BooksModule],
+  imports: [ConfigModule.forRoot(), UserModule,BooksModule, LibraryModule],
   controllers: [AppController],
   providers: [],
 })
