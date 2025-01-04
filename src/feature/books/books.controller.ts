7031baaa897bf221f3223d9fa8123daf55da8598
Hector <hectikorf@gmail.com>
2025-01-03 20:31:02 -0500

    Books crud ok

--- src/feature/books/books.controller.ts
+++ src/feature/books/books.controller.ts
@@ -1,41 +1,47 @@
-import { Body, Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { BooksService } from './books.service';
-
 import { Book } from './entity/books.entity';
-import { BookModel } from './model/books.model';
-import { CreateUserDto } from '../user/dto/create-user.dto';
 import { CreateBook } from './dto/create-book.dto';
+import { UpdateBook } from './dto/update-book.dto';
 
 @ApiTags('Books')
 @Controller('books')
 export class BooksController {
-   
+  constructor(private readonly booksService: BooksService) {}
 
-   constructor(private booksService:BooksService){}
   @Get()
+  @ApiOkResponse({ type: [Book] })
   getBooks() {
-   return this.booksService.getBooks();
+    return this.booksService.findAll();
+  }
+
+  @Get(':id')
+  @ApiOkResponse({ type: Book })
+  getBook(@Param('id') id: number) {
+    return this.booksService.findOne(+id);
   }
 
   @Post()
-  @ApiOkResponse({type:Book})
-  createBook(@Body()books :CreateBook){
-    
-    return this.booksService.create(books);
+  @ApiOkResponse({ type: Book })
+  createBook(@Body() createBook: CreateBook) {
+    return this.booksService.create(createBook);
+  }
 
+  @Put(':id')
+  @ApiOkResponse({ type: Book })
+  updateBook(@Param('id') id: number, @Body() updateBook: UpdateBook) {
+    return this.booksService.update(+id, updateBook);
   }
-  @Put()
-  updateBook(){
-    return this.booksService.updatebooks();
+
+  @Patch(':id')
+  @ApiOkResponse({ type: Book })
+  partialUpdateBook(@Param('id') id: number, @Body() updateBook: UpdateBook) {
+    return this.booksService.update(+id, updateBook);
   }
-  @Patch()
-  patchBook(){
-    return this.booksService.updateBooksStatus();
-  }
-  
-  @Delete()
-  deleteBook(){
-    return this.booksService.deletebooks();
+
+  @Delete(':id')
+  deleteBook(@Param('id') id: number) {
+    this.booksService.remove(+id);
   }
 }
