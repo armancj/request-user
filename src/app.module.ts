2cc8e02a1002c4a3b7856e008636beb296abae1d
mandi <armancanjer@gmail.com>
2025-01-06 11:18:58 -0500

    Add EditorialModule to AppModule imports
    Included EditorialModule in the AppModule to integrate new editorial features. Updated the import list for better readability and maintainability.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,18 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './feature/user/user.module';
 import { LibraryModule } from './feature/library/library.module';
 import { AuthorsModule } from './feature/authors/authors.module';
+import { EditorialModule } from './feature/editorial/editorial.module';
 
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule,BooksModule, LibraryModule,AuthorsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UserModule,
+    BooksModule,
+    LibraryModule,
+    AuthorsModule,
+    EditorialModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
