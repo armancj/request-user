952e31130138fb05bfa2f960f21bac5c4cd22e7e
mandi <armancanjer@gmail.com>
2025-01-10 16:13:37 -0500

    Fix typo in user model property in test file
    Renamed "edad" to "age" in the test case to match the correct property name in the UserModel. This ensures consistency and prevents potential confusion or mismatches in the test implementation.

--- src/feature/user/entity/user.entity.ts
+++ src/feature/user/entity/user.entity.ts
@@ -1,13 +1,18 @@
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
 import { UserModel } from '../model/user.model';
 import { ApiProperty } from '@nestjs/swagger';
 
+@Entity()
 export class User implements UserModel {
   @ApiProperty()
-  id: number;
+  @PrimaryKey({ autoincrement: true })
+  id!: number;
 
   @ApiProperty()
-  edad: number;
+  @Property({ type: 'integer' })
+  age: number;
 
   @ApiProperty()
+  @Property({ type: 'string' })
   name: string;
 }
