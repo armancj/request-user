473524ff3c9f14eeb4d82bd3c3bc37e5b1501e67
mandi <armancanjer@gmail.com>
2025-01-13 10:02:05 -0500

    Set default values and remove unused ApiProperty decorator
    Added default values for `age` and `name` in user-related classes to improve initialization consistency. Removed unused `ApiProperty` decorator references, as they were redundant with the Swagger plugin now defined in `nest-cli.json`. This simplifies the code and reduces dependencies.

--- src/feature/user/entity/user.entity.ts
+++ src/feature/user/entity/user.entity.ts
@@ -1,18 +1,14 @@
 import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
 import { UserModel } from '../model/user.model';
-import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class User implements UserModel {
-  @ApiProperty()
   @PrimaryKey({ autoincrement: true })
   id!: number;
 
-  @ApiProperty()
   @Property({ type: 'integer' })
-  age: number;
+  age: number = 18;
 
-  @ApiProperty()
   @Property({ type: 'string' })
-  name: string;
+  name: string = `mandi`;
 }
